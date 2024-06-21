69dbccdca897bd916dc4cf24c81af191be77e6c4
halfdobis <missdobis@gmail.com>
2024-06-21 23:42:28 +0100

    contact modal

--- src/components/modals/contact.tsx
+++ src/components/modals/contact.tsx
@@ -37,7 +37,7 @@ const ContactForm: React.FC<ContactFormProps> = ({ closeModal }) => {
         <Card className="relative rounded-lg shadow-lg p-8 max-w-md w-full">
           <button
             onClick={closeModal}
-            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+            className="absolute top-2 right-4 text-2xl"
           >
             &times;
           </button>
