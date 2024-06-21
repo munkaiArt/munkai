905519a3dfbe1215d3a87a8afa3463045212de5e
halfdobis <missdobis@gmail.com>
2024-06-21 23:39:28 +0100

    contact modal

--- src/components/landingPage/footer.tsx
+++ src/components/landingPage/footer.tsx
@@ -1,105 +1,134 @@
-import React from "react";
-import MunkaiLogo from "../../../public/images/munkai-logo.png";
+"use client";
+import React, { useState } from "react";
 import MunkaiLogoDark from "../../../public/images/monkai-logo-white.png";
 import Link from "next/link";
 import Image from "next/image";
+import ContactForm from "../modals/contact";
+import { Button } from "../ui/button";
 
 const Footer: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
   const litepaper = "../../../public/documents/litepaper.pdf";
 
   return (
-    <section className="bg-secondary mt-20 border-t-2 border-primary">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-        <div className="flex flex-col md:flex-row justify-between space-y-8 md:space-y-0">
-          <div className="md:w-1/4 flex flex-col space-y-4">
-            <Image
-              src={MunkaiLogoDark}
-              alt="Munkai"
-              className="h-6 w-24 md:h-8 md:w-32"
-            />
-            <p className="text-sm">
-              Creating a Read-to-Earn (R2E) platform on the blockchain for
-              readers, crypto enthusiasts, and creators.
-            </p>
-          </div>
+    <>
+      {open && <ContactForm closeModal={() => setOpen(false)} />}
 
-          <div className="flex flex-col space-y-4 md:space-y-0">
-            <p className="font-semibold uppercase">Social Links</p>
-            <div className="flex flex-col text-primary text-sm space-y-2">
-              <Link
-                href="https://t.me/munkaiArt"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Telegram
-              </Link>
-              <Link
-                href="https://twitter.com/munkaiart?t=UkGK2f-YT799rXXMfvIyuQ&s=09"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Twitter
-              </Link>
-              <Link
-                href="https://instagram.com/munkaiart?utm_medium=copy_link"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Instagram
-              </Link>
-              <Link
-                href="https://youtube.com/channel/UCgpv6rt9FQwOhXa8A0ij-_A"
-                target="_blank"
-                rel="noreferrer"
-              >
-                YouTube
-              </Link>
+      <section className="bg-secondary mt-20 border-t-2 border-primary">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+          <div className="flex flex-col md:flex-row justify-between space-y-8 md:space-y-0">
+            <div className="md:w-1/4 flex flex-col space-y-4">
+              <Image
+                src={MunkaiLogoDark}
+                alt="Munkai"
+                className="h-6 w-24 md:h-8 md:w-32"
+              />
+              <p className="text-sm">
+                Creating a Read-to-Earn (R2E) platform on the blockchain for
+                readers, crypto enthusiasts, and creators.
+              </p>
             </div>
-          </div>
 
-          <div className="flex flex-col space-y-4 md:space-y-0">
-            <p className="font-semibold uppercase">Contact Us</p>
-            <div className="flex flex-col text-primary text-sm space-y-2">
-              <Link href="mailto:hello@munkai.art">hello@munkai.art</Link>
-              <Link href="/contact">Contact Us</Link>
+            <div className="flex flex-col space-y-4 md:space-y-0">
+              <p className="font-semibold uppercase mb-2">Social Links</p>
+              <div className="flex flex-col text-sm space-y-2">
+                <Link
+                  href="https://t.me/munkaiArt"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover:text-primary transition-all duration-500"
+                >
+                  Telegram
+                </Link>
+                <Link
+                  href="https://twitter.com/munkaiart?t=UkGK2f-YT799rXXMfvIyuQ&s=09"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover:text-primary transition-all duration-500"
+                >
+                  Twitter
+                </Link>
+                <Link
+                  href="https://instagram.com/munkaiart?utm_medium=copy_link"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover:text-primary transition-all duration-500"
+                >
+                  Instagram
+                </Link>
+                <Link
+                  href="https://youtube.com/channel/UCgpv6rt9FQwOhXa8A0ij-_A"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="hover:text-primary transition-all duration-500"
+                >
+                  YouTube
+                </Link>
+              </div>
             </div>
-          </div>
 
-          <div className="flex flex-col space-y-4 md:space-y-0 md:w-1/4">
-            <p className="font-semibold uppercase">Legal</p>
-            <div className="flex flex-col text-primary text-sm space-y-2">
-              <Link href="/terms" target="_blank" rel="noopener noreferrer">
-                Terms and conditions
-              </Link>
-              <Link
-                href={litepaper}
-                download
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Litepaper
-              </Link>
-              <Link
-                href="https://github.com/cyberscope-io/audits/blob/main/munk/audit.pdf"
-                download
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Audit Report
-              </Link>
+            <div className="flex flex-col space-y-4 md:space-y-0">
+              <p className="font-semibold uppercase mb-2">Contact Us</p>
+              <div className="flex flex-col text-sm space-y-2">
+                <Link
+                  href="mailto:hello@munkai.art"
+                  className="hover:text-primary transition-all duration-500"
+                >
+                  hello@munkai.art
+                </Link>
+                <button
+                  onClick={() => setOpen(true)}
+                  className="text-start hover:text-primary transition-all duration-500"
+                >
+                  Contact Us
+                </button>
+              </div>
+            </div>
+
+            <div className="flex flex-col space-y-4 md:space-y-0 md:w-1/4">
+              <p className="font-semibold uppercase mb-2">Legal</p>
+              <div className="flex flex-col text-sm space-y-2">
+                <Link
+                  href="/terms"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-primary transition-all duration-500"
+                >
+                  Terms and conditions
+                </Link>
+                <Link
+                  href={litepaper}
+                  download
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-primary transition-all duration-500"
+                >
+                  Litepaper
+                </Link>
+                <Link
+                  href="https://github.com/cyberscope-io/audits/blob/main/munk/audit.pdf"
+                  download
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-primary transition-all duration-500"
+                >
+                  Audit Report
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
-        <hr className="my-4 border-primary" />
-        <p className="text-sm text-center">
-          &copy; {new Date().getFullYear()}{" "}
-          <span className="text-primary font-semibold">Munkai</span>. All rights
-          reserved.
-        </p>
-      </div>
-    </section>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+          <hr className="my-4 border-primary" />
+          <p className="text-sm text-center">
+            &copy; {new Date().getFullYear()}{" "}
+            <span className="text-primary font-semibold">Munkai</span>. All
+            rights reserved.
+          </p>
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/components/modals/contact.tsx
+++ src/components/modals/contact.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
@@ -19,56 +19,69 @@ type ContactFormValues = {
   message: string;
 };
 
-const ContactForm: React.FC = () => {
+type ContactFormProps = {
+  closeModal: () => void;
+};
+
+const ContactForm: React.FC<ContactFormProps> = ({ closeModal }) => {
   const methods = useForm<ContactFormValues>();
 
   const onSubmit: SubmitHandler<ContactFormValues> = (data) => {
     console.log(data);
+    closeModal();
   };
 
   return (
-    <FormProvider {...methods}>
-      <Card className="relative rounded-lg shadow-lg p-8 max-w-md w-full">
-        <h2 className="text-2xl font-bold text-center mb-6">Contact Us</h2>
-        <FormDescription className="mb-6 px-4 md:px-10 mt-2 text-center">
-          Have any questions, feedback, or inquiries? We'd love to hear from
-          you. Feel free to reach out to us using the form below.
-        </FormDescription>
-        <form onSubmit={methods.handleSubmit(onSubmit)} className="space-y-4">
-          <FormField
-            name="email"
-            control={methods.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="Enter your email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            name="message"
-            control={methods.control}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Message</FormLabel>
-                <FormControl>
-                  <Textarea />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <div className="flex items-center justify-center">
-            <Button className="font-semibold" type="submit">
-              Send
-            </Button>
-          </div>
-        </form>
-      </Card>
-    </FormProvider>
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-50 transition-opacity bg-black dark:bg-opacity-50">
+      <FormProvider {...methods}>
+        <Card className="relative rounded-lg shadow-lg p-8 max-w-md w-full">
+          <button
+            onClick={closeModal}
+            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          >
+            &times;
+          </button>
+          <h2 className="text-2xl font-bold text-center mb-6">Contact Us</h2>
+          <FormDescription className="mb-6 px-4 md:px-10 mt-2 text-center">
+            Have any questions, feedback, or inquiries? We'd love to hear from
+            you. Feel free to reach out to us using the form below.
+          </FormDescription>
+          <form onSubmit={methods.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              name="email"
+              control={methods.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter your email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="message"
+              control={methods.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Message</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="Enter your message" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center justify-center">
+              <Button className="font-semibold" type="submit">
+                Send
+              </Button>
+            </div>
+          </form>
+        </Card>
+      </FormProvider>
+    </div>
   );
 };
 
