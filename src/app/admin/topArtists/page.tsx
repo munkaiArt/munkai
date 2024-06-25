672864986968742af4f19f772c69561654c9ff46
halfdobis <missdobis@gmail.com>
2024-06-25 14:22:15 +0100

    admin topArtists page

---
+++ src/app/admin/topArtists/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from "react";
+
+export default function TopArtists() {
+  return (
+    <main className="p-8">
+      <div>
+        <div>
+          <h1 className="text-xl my-4">Top Artists Page</h1>
+        </div>
+      </div>
+    </main>
+  );
+}
