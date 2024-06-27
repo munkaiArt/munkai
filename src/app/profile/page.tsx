bd2cc5d9732de1126a8cb6c30f797fad1259570e
halfdobis <missdobis@gmail.com>
2024-06-27 09:32:40 +0100

    profile function

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,15 +1,21 @@
 "use client";
-import AdminProfile from "@/components/profiles/adminProfile";
 import CreatorProfile from "@/components/profiles/creatorProfile";
 import UserProfile from "@/components/profiles/userProfile";
 import React from "react";
 
 export default function Profile() {
+  const isCreator = true;
   return (
     <div>
-      <UserProfile />
-      {/* <CreatorProfile /> */}
-      {/* <AdminProfile /> */}
+      {!isCreator ? (
+        <>
+          <UserProfile />
+        </>
+      ) : (
+        <>
+          <CreatorProfile />
+        </>
+      )}
     </div>
   );
 }
